1a426be611d48d8eca73ec9c1627e746caf5b89f
BIT <BIT@DESKTOP-SU5SFCR>
2019-03-22 13:51:21 +0900

    update

--- src/main/java/com/douzone/jblog/controller/UserController.java
+++ src/main/java/com/douzone/jblog/controller/UserController.java
@@ -31,12 +31,6 @@ public class UserController {
 	@Autowired
 	private CategoryService categoryService;
 	
-	@RequestMapping(value="/auth", method=RequestMethod.POST)
-	public void auth() {}
-	
-	@RequestMapping(value="/logout", method=RequestMethod.GET)
-	public void logout() {}
-	
 	@RequestMapping(value="join", method=RequestMethod.GET)
 	public String join(@ModelAttribute UserVo userVo) {
 		return "user/join";
